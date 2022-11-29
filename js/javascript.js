bbea15bedc79b12cfcd2c2319aa48e64245e0968
aguscuesta13 <98561790+aguscuesta13@users.noreply.github.com>
2022-11-29 16:30:05 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+function modooscuro(){
+    document.body.style.backgroundImage="none";
+    document.body.style.backgroundColor="black";
+    document.body.style.background.color="white"
+}
+
+function creador(){
+    document.getElementById("luis").innerHTML = "Agustina Cuesta-Programadora web, since 2000";
+}
---
+++ js/script.js
@@ -0,0 +1,2 @@
+function fondooscuro(){document.body.style.backgroundImage = "black";}
+
